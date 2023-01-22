3af454b06b0c2ce71723857f29d2e418417a5119
Tom Tom <70383056+pajakbalikpapan@users.noreply.github.com>
2023-01-22 08:26:26 +0800

    Update index.tsx

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-screen bg-black">
+    <div className="flex h-screen bg-gray-400">
       <Head>
         <title>Pajak365 | Diskusi pajak pertama dan terbesar di Indonesia</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="m-auto w-3xl">
+      <div className="m-auto max-w-5xl">
         <Image
           width={512}
           height={512}
